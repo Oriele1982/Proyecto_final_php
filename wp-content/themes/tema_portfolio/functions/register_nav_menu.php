5264bee24913278562c25355788ae8f292a6ea98
Oriele1982 <oriele.no@hotmail.com>
2018-08-23 19:53:07 -0300

    primer commit php

--- wp-content/themes/tema_portfolio/functions/register_nav_menu.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-	register_nav_menus( array(
-		'header-menu' => __( 'Header Menu' )
-	));
-
-
- ?>
--- wp-content/themes/tema_portfolio/functions/the_theme_file_uri.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    function the_theme_file_uri($file) {
-        echo get_theme_file_uri($file);
-    }
